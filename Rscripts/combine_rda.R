f7dfafbf5e70022628a0ec266213fb1f6d97bd32
Andrew Dunford <adunford@broadinstitute.org>
2017-03-30 18:50:24 +0000

    applying fixes to combine_rda.R

--- Rscripts/combine_rda.R
+++ Rscripts/combine_rda.R
@@ -5,21 +5,22 @@
 
 
 library("optparse")
+library(BiocGenerics)
 
 option_list = list(
-	    make_option(c("-r","-rdaFiles"),type='charcter',deafult=NULL,
-	      help="Output from pileometh/methydackel")
-	    make_option(c("-o","-outFile"),type='character',deafult=NULL,
+	    make_option(c("-r","--rdaFiles"),type='character',default=NULL,
+	      help="Output from pileometh/methydackel"),
+	    make_option(c("-o","--outFile"),type='character',default=NULL,
 	      help="Names of the output file")
 
 	      )
-opt_parse=OptionParse(options_list=option_list)
+opt_parse=OptionParser(option_list=option_list)
 opt = parse_args(opt_parse)
 
-rdaList = unlist(strsplit(opt$rdaFiles),',')
+rdaList = unlist(strsplit(opt$rdaFiles,','))
 message("Combining bs object")
 
-bs<-Reduce(combine,rdaList)
+bs<-Reduce(BiocGenerics::combine,rdaList)
 bs@parameters<-rdaList[[1]]@parameters
 
 
