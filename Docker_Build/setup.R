26f7d9260ff6343c9b4c781b7e615a47ecedb592
ajdunford <adunford@broadinstitute.org>
2017-05-24 20:41:17 +0000

    Adding step to dockerfile to run setup.R to install needed packages

--- Docker_Build/setup.R
+++ Docker_Build/setup.R
@@ -3,3 +3,11 @@ biocLite('bsseq');
 biocLite('BiocGenerics');
 
 install.packages("optparse", repos="http://R-Forge.R-project.org")
+install.packages("Repitools",repos="http://R-Forge.R-project.org")
+install.packages("ggplot2",repos="http://R-Forge.R-project.org")
+
+install.packages("yaml",repos="http://R-Forge.R-project.org")
+
+install.packages("foreach",repos="http://R-Forge.R-project.org")
+install.packages("devtools",repos="http://R-Forge.R-project.org")
+install.packages("Repitools",repos="http://R-Forge.R-project.org")
