47f26945822ebe02521717a8a2b28ec2cf894b35
Martin Aryee <martin.aryee@gmail.com>
2017-08-30 11:24:21 -0400

    Export betas as tsv

--- array/preprocess_450k.R
+++ array/preprocess_450k.R
@@ -6,14 +6,16 @@ sample_sheet <- "testdata/450k/samples.csv"
 qc_intensity_cutoff <- 11
 
 # OUTPUT
-qc_file <- "qc.csv"
-preprocesed_genomic_ratio_set <- "gmset.rds"
+qc_file <- "qc.tsv"
+betas_file <- "beta.tsv"
+preprocesed_genomic_ratio_set_rds <- "gmset.rds"
 
 targets <- read.csv(sample_sheet, stringsAsFactors = FALSE)
 targets$Basename <- file.path(idat_base_dir, targets$Basename)
 
 # This reads the files specified in the Basename column of targets
 rgset <- read.metharray.exp(targets=targets)
+sampleNames(rgset) <- targets[,1]
 
 # QC
 mset <- preprocessRaw(rgset) 
@@ -21,10 +23,14 @@ qc <- cbind(pData(rgset), getQC(mset))
 bad_idx <- qc$mMed < qc_intensity_cutoff
 badsamples <- rownames(qc)[bad_idx] 
 qc$qc_pass <- ifelse(sampleNames(mset) %in% badsamples, "FAIL", "PASS")
-plotQC(qc, badSampleCutoff = cutoff)
-write.csv(qc, file=qc_file)
+plotQC(qc, badSampleCutoff = qc_intensity_cutoff)
 
-# Preprocess
+# Preprocess and get betas
 grset_funnorm <- preprocessFunnorm(rgset[,!bad_idx], nPCs=2)  
-saveRDS(grset_funnorm, preprocesed_genomic_ratio_set)  
+b <- getBeta(grset_funnorm)
+
+# Save output files
+write.table(qc, file=qc_file, sep="\t", quote=FALSE, row.names=FALSE)
+write.table(b, file=betas_file, sep="\t", quote=FALSE, col.names=NA)
+saveRDS(grset_funnorm, preprocesed_genomic_ratio_set_rds)  
 
