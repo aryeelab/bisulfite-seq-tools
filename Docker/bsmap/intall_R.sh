ba985af9be0be0a4271117a416cd111b58ba788e
Martin Aryee <martin.aryee@gmail.com>
2017-12-28 22:37:12 -0500

    Removed unused Docker/bsmap dir

--- Docker/bsmap/intall_R.sh
+++
@@ -1,2 +0,0 @@
-export LDFLAGS="-L/usr/local/packages6/libs/gcc/4.4.7/bzip2/1.0.6/lib/"
-./configure --with-readline=no --with-x=no --libdir=/usr/local/packages6/libs/gcc/4.4.7/bzip2/1.0.6/lib/ --enable-R-shlib --with-blas --with-lapack
